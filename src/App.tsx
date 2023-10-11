bcf73e78e861309e2e2befad1726c6577fc72b19
Dav.O <davolufuwa@gmail.com>
2023-10-11 20:24:25 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
+import Layout from "./components/Layout"
+import Profile from "./pages/Profile"
+import Homepage from "./pages/Homepage"
+
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index  element={<Homepage />} />
+      <Route path="techies" element={<Profile />}>
+      
+      </Route>
+    </Route>  
+  ))
+
+function App() {
+  return (
+    <RouterProvider router={routes}>
+    </RouterProvider>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,69 @@
+
+
+const Hero = () => {
+  return (
+    <div className='relative min-h-[100svh] bg-black text-white text-sm md:text-base'>
+      <h1 className='glow absolute top-[50%] md:top-[45%] w-full text-slate-200 uppercase text-6xl md:text-7xl font-black  text-center'>Share your tech story</h1>
+
+      <div className='absolute px-3 bottom-[20%] md:bottom-[30%] left-0 w-24 sm:min-w-max'>
+        Tech is pretty interesting in the real world
+      </div>
+      <div className="absolute px-3 top-0 right-0 w-40 text-right">
+        You need to know data structures and algorithms to get into the big 6. 
+      </div>
+      <div className="absolute px-3 top-[20%] right-0 text-right">
+        Not just a developer or designer!<br />
+        A problem solver! 
+      </div>
+      <div className="absolute px-3 top-[22%] left-0 w-36">
+        Why did I even get into tech in the first place?
+      </div>
+      <div className="absolute px-3 top-[40%] right-0 w-40 text-right">
+        You may not get that six figure job until probably 2 years after
+      </div>
+      <div className="absolute px-3 bottom-[15%] right-0 w-40 text-right">
+        This has been actual fun, I love my job!. I am glad I got into tech. This journey has been exciting
+      </div>
+      <div className="absolute px-3 bottom-0 right-0">
+        Lets get this money!!!
+      </div>
+      <div className="absolute px-3 bottom-0 left-0">
+        <b className="uppercase font-extrabold">OpenSource</b>
+      </div>
+      <div className="absolute px-3 left-0 bottom-[7%]">
+        300 rejections<br/> 
+        12 interviews<br/> 
+        6 final stages<br/> 
+        1 <b>YES</b><br/> 
+      </div>
+      <div className="absolute px-3 left-0 top-[34%] md:top-[40%]">
+        Frontend developer<br/> 
+        Backend developer<br/> 
+        Full stack developer<br/> 
+        Data Analyst<br/> 
+        UI/UX Designer<br/> 
+      </div>
+      <div className="absolute px-3 left-0 top-0">
+        QA Engineer<br/> 
+        AI/ML Engineer<br/> 
+        Technical Writer<br/> 
+        Cloud Engineer<br/> 
+        DevOps Engineer<br/> 
+        CyberSecurity Expert<br/> 
+      </div>
+      <div className="absolute px-3 left-[38%] bottom-0 text-center hidden md:block">
+        Consistency is key, hard work pays off. <br/>
+        You need to have a great portfolio to get into the big 6.<br />
+        But I got my first job in tech using a twitter clone. <br />
+        This is <b>2023</b><br />
+      </div>
+      <div className="absolute px-3 left-[38%] top-0 text-center hidden md:block">
+        Remember that the tech industry is constantly evolving,<br/> 
+        so continuous learning and adaptability are key.<br/> 
+        You've got this!
+      </div>
+    </div>
+  )
+}
+
+export default Hero
